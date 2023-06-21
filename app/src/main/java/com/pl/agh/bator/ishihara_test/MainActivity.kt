ad69d1dafa438c61cd14a54b81b2d2480b32a09b
Ryszard Błażej <sapermysza@gmail.com>
2023-06-21 13:39:10 +0200

    .xml files for each layout added with vital controls

--- app/src/main/java/com/pl/agh/bator/ishihara_test/MainActivity.kt
+++ app/src/main/java/com/pl/agh/bator/ishihara_test/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        setContentView(R.layout.choose_mode)
     }
 }
